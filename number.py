f386ecfe14bcd7796e986a19750cd37b1bb2d094
Pacifier25 <piyushbist10@gmail.com>
2020-08-01 12:35:26 +0530

    modifying numers

--- number.py
+++ number.py
@@ -2,6 +2,17 @@ print(type(1)) #int
 print(type(0.0)) #int
 print(10 ** 3 ) #power
 #string conversion to number
-age = input('what\'s your age')
-age = int(age)
-print(age)
+#age = input('what\'s your age')
+#age = int(age)
+#print(age)
+print(len('turtle'))
+name = 'hi there'
+print(name)
+print(name.strip('h'))
+print(name.split())
+print(name.replace('hi','hello'))
+print(name.startswith('hi'))
+print(name.endswith('hi'))
+print(name.upper())
+print(name.find('h'))
+print(name.count('e'))
